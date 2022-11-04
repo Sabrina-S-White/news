bba0c26cb384d2a88a544a2af0ca4202701920f0
sabrinaMac <sabrina.scherma@gmail.com>
2022-11-04 00:15:25 -0600

    worked on main.html

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+const sidebarBtn = document.getElementById('sidebarBtn');
+const sidebar = document.getElementById('sidebar');
+sidebarBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebarShow');
+})
