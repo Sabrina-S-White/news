61a688cea120fd2c27d209623a098a42e554533c
sabrinaMac <sabrina.scherma@gmail.com>
2022-11-15 23:59:55 -0700

    worked on dist creation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-const sidebarBtn = document.getElementById('sidebarBtn');
+function sidebarF() {
+  const sidebarBtn = document.getElementById('sidebarBtn');
 const sidebar = document.getElementById('sidebar');
-sidebarBtn.addEventListener('click', () => {
-  console.log('ok')
+sidebarBtn.addEventListener('click', function () {
   sidebar.classList.toggle('sidebarShow');
 })
+}
 
-const flags = document.querySelectorAll('.flag');
-flags.forEach(flag => {
-  flag.addEventListener('click', () => {
-    flag.classList.toggle('flag-style-red')
-    console.log(flag.parentElement.firstChild)
-  })
-})
+
+// const flags = document.querySelectorAll('.flag');
+// flags.forEach(flag => {
+//   flag.addEventListener('click', () => {
+//     flag.classList.toggle('flag-style-red')
+//   })
+// })
